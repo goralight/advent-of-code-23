f200a0d2035f0be184a8814bf7e3e3646f229606
Noor <hussainmahnoor112@gmail.com>
2024-12-21 12:14:36 +0000

    Updated code based on feedback

--- src/components/Slug/Slug.tsx
+++ src/components/Slug/Slug.tsx
@@ -3,16 +3,13 @@ import styled from '@emotion/styled'
 
 const Container = styled.div`
   background-color: #ffffff;
-  width: 100%;
   padding: 30px 46px;
 `
 const InputContainer = styled.div`
   display: flex;
   flex-direction: column;
-  text-align: left;
   background-color: #EDEDED;
   border: none;
-  width: auto;
   padding: 25px 26px;
   border: 4px solid #0015FF;
   border-radius: 10px;
@@ -30,7 +27,6 @@ const Label = styled.label`
 const StyledInput = styled.input`
   background-color: #EDEDED;
   border: none;
-  width: auto;
   height: 10px;
   padding: 20px;
   border: none;
@@ -54,16 +50,14 @@ const Slug = (): JSX.Element => {
   
   const slug = value.trim().length > 0 ? `/${value.trim().split(' ').join('-').toLowerCase()}` : '';
   
-  const handleValueChange = (e:any):void => {
-    console.log('e', e.target.value)
-    console.log('Input Value:', e.target.value);
+  const handleValueChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
     setValue(e.target.value)
   }
 
   return (
     <Container>
       <InputContainer>
-        <Label htmlFor="titleInput">Title</Label>
+        <Label htmlFor='titleInput'>Title</Label>
         <StyledInput value={value} onChange={handleValueChange}/> 
       </InputContainer>
       {slug && (
@@ -73,7 +67,6 @@ const Slug = (): JSX.Element => {
       )}
     </Container>
   )
-  
 }
   
 export default Slug
